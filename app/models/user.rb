c0ac89634b1441a92b648247c2a07f66bbca6593
TruongNV <truongnv105>
2018-06-12 13:54:00 +0700

    Finish sign up, edit with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
   has_many :chat_rooms
   has_many :messages
+
+  validates :name, presence: true, length: {minimum: Settings.users.name.min_length,
+    maximum: Settings.users.name.max_length}
+  validates :address, presence: true, length: {minimum: Settings.users.address.min_length}
+  validates :phone_number, presence: true, length: {minimum: Settings.users.phone_number.min_length,
+    maximum: Settings.users.phone_number.max_length}
 end
