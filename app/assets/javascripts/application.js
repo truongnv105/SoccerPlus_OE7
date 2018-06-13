ba2115f3d5e22c2f601062b4c4f259d0e3c9e6ed
nqngoc198 <nguyenquangngoc1981995@gmail.com>
2018-06-13 11:22:59 +0700

    news page users

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require custom
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   root "static_pages#index"
   devise_for :users
-
   devise_scope :user do
     get "/signup", to: "devise/registrations#new"
     get "/login", to: "devise/sessions#new"
     delete "logout", to: "devise/sessions#destroy"
   end
-
   resources :users, only: :show
+  resources :news, only: :index
 end
