66320fddf93f8c710684429725adac127de822cd
nqngoc198 <nguyenquangngoc1981995@gmail.com>
2018-06-21 16:10:29 +0700

    like facebook

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   mount ActionCable.server => "/cable"
 
-  root "static_pages#index"
+  root "news#index"
   devise_for :users,
     controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   devise_scope :user do
